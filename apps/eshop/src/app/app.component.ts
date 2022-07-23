2b079ab5f5067eb25af354c045c4b7274c12d37c
Jose <josebratt@gmail.com>
2022-07-23 17:00:32 +0200

    Update app.component.ts

--- apps/eshop/src/app/app.component.ts
+++ apps/eshop/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'eshop-root',
+  selector: 'tutorial-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
