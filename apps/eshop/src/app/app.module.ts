f923f588ba372fa168d3f72f58871a18d5862758
Jose <josebratt@gmail.com>
2022-07-23 16:04:12 +0200

    Update app.module.ts

--- apps/eshop/src/app/app.module.ts
+++ apps/eshop/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+import { Routes, RouterModule } from '@angular/router';
+import { ProductlistComponent } from './pages/productlist/productlist.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'products', component: ProductlistComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    HomePageComponent,
+    ProductlistComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
