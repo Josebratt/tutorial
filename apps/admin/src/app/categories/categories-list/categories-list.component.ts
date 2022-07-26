155523957dd9d92ce23e4a02a6442e3dc9783354
Jose <josebratt@gmail.com>
2022-07-26 12:38:07 -0400

    Update categories-list.component.ts

--- apps/admin/src/app/categories/categories-list/categories-list.component.ts
+++ apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -1,16 +1,22 @@
+import { CategoriesService, Category } from '@tutorial/products';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'tutorial-categories-list',
+  selector: 'admin-categories-list',
   templateUrl: './categories-list.component.html',
   styles: [
   ]
 })
 export class CategoriesListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private categoriesService:CategoriesService) { }
 
   ngOnInit(): void {
+    this.categoriesService.getCategories().subscribe(
+      cats => this.categories = cats
+    )
   }
 
 }
