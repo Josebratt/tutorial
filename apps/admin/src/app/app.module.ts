ea9a2b37a44afd589e173c91e35231f28d3baff1
Jose <josebratt@gmail.com>
2022-07-26 22:19:59 -0400

    Update app.module.ts

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
 
 import { AppComponent } from './app.component';
@@ -15,12 +19,18 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 
 const UX_MODULE = [
   ButtonModule,
+  ConfirmDialogModule,
   CardModule,
   TableModule,
+  InputTextModule,
+  ToastModule,
   ToolbarModule
 ]
 
@@ -31,7 +41,8 @@ const routes: Routes = [
     component: ShellComponent,
     children: [
       { path: 'dashboard', component: DashboardComponent },
-      { path: 'categories', component: CategoriesListComponent }
+      { path: 'categories', component: CategoriesListComponent },
+      { path: 'categories/form', component: CategoriesFormComponent }
     ]
   }
 ];
@@ -44,14 +55,18 @@ const routes: Routes = [
     ShellComponent,
     SidebarComponent,
     CategoriesListComponent,
+    CategoriesFormComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     ...UX_MODULE
   ],
-  providers: [],
+  providers: [MessageService, ConfirmationService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
