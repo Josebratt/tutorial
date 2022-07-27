d863c21f1df5484e66e1cef2d424134bf7fec035
Jose <josebratt@gmail.com>
2022-07-26 22:20:59 -0400

    Update categories.service.ts

--- libs/products/src/lib/services/categories.service.ts
+++ libs/products/src/lib/services/categories.service.ts
@@ -8,9 +8,17 @@ import { Category } from '../models/category';
 })
 export class CategoriesService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   getCategories(): Observable<Category[]> {
-    return this.httpClient.get<Category[]>('http://localhost:3000/api/v1/categories');
+    return this.http.get<Category[]>('http://localhost:3000/api/v1/categories');
+  }
+
+  createCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>('http://localhost:3000/api/v1/categories', category);
+  }
+
+  deleteCategory(categoryId: string): Observable<Category> {
+    return this.http.delete<Category>(`http://localhost:3000/api/v1/categories/${categoryId}`);
   }
 }
