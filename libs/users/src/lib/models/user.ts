7e38e5216c09fd6479098db5230c5ab49f285e04
Jose <josebratt@gmail.com>
2022-07-30 15:46:53 -0400

    Update user.ts

--- libs/users/src/lib/models/user.ts
+++ libs/users/src/lib/models/user.ts
@@ -1,8 +1,8 @@
 export class User {
     id?: string;
-    name?: string;
-    password?: string;
-    email?: string;
+    name='';
+    password='';
+    email='';
     phone?: string;
     token?: string;
     isAdmin?: true;
