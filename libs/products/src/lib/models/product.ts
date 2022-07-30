a76f08aeb7d05ba4aa26a4992d5e46f078a9443f
Jose <josebratt@gmail.com>
2022-07-30 15:46:48 -0400

    Update product.ts

--- libs/products/src/lib/models/product.ts
+++ libs/products/src/lib/models/product.ts
@@ -2,13 +2,13 @@ import { Category } from './category';
 
 export class Product {
   id?: string;
-  name?: string;
+  name = '';
   description?: string;
   richDescription?: string;
   image?: string;
   images?: string[];
   brand?: string;
-  price?: number;
+  price = 0 ;
   category?: Category;
   countInStock?: number;
   rating?: number;
