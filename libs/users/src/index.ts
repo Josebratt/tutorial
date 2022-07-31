a02830b26d81a81378eee4265a52d8aa9694027c
Jose <josebratt@gmail.com>
2022-07-31 11:04:42 -0400

    Update index.ts

--- libs/users/src/index.ts
+++ libs/users/src/index.ts
@@ -1,4 +1,6 @@
 export * from './lib/users.module';
 export * from './lib/models/user';
 export * from './lib/services/users.service';
+export * from './lib/services/auth.service'
 export * from './lib/services/auth-guard.guard';
+export * from './lib/services/jwt.interceptor';
