7ade3de98ea377cb2b557a84c2a5c28f2ac2b425
Jose <josebratt@gmail.com>
2022-07-31 11:04:47 -0400

    Update auth.service.ts

--- libs/users/src/lib/services/auth.service.ts
+++ libs/users/src/lib/services/auth.service.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { LocalstorageService } from './localstorage.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@env/environment';
@@ -13,10 +15,17 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
+    private localstorageService: LocalstorageService,
+    private router: Router
   ) { }
 
   login(email: string, passwordHash: string): Observable<User> {
     return this.http.post<User>(`${this.apiURLUsers}/login`, { email, passwordHash });
+  }
+
+  logout() {
+    this.localstorageService.removeToken();
+    this.router.navigate(['/login']);
   }
 
 }
