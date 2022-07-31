eb57305daa6bd2ca0866c4903039f51f51b1e7ba
Jose <josebratt@gmail.com>
2022-07-31 17:39:55 -0400

    Update app.module.ts

--- apps/eshop/src/app/app.module.ts
+++ apps/eshop/src/app/app.module.ts
@@ -36,7 +36,7 @@ const routes: Routes = [
     UiModule,
     ProductsModule,
     BrowserAnimationsModule,
-    AccordionModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent],
