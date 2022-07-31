8a29daed46fb695785cf6dbfb1af2c2431628509
Jose <josebratt@gmail.com>
2022-07-31 16:05:02 -0400

    Update products.module.ts

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductsSearchComponent } from './components/products-search/products-search.component';
 
 @NgModule({
   imports: [CommonModule],
+  exports: [ProductsSearchComponent],
+  declarations: [
+    ProductsSearchComponent
+  ],
 })
 export class ProductsModule {}
