d15629ac4d9a81d3add3ad4d0616abe17fe828ec
Jose <josebratt@gmail.com>
2022-07-31 11:04:08 -0400

    Update products-list.component.ts

--- apps/admin/src/app/pages/products/products-list/products-list.component.ts
+++ apps/admin/src/app/pages/products/products-list/products-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product, ProductsService } from '@tutorial/products';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'admin-products-list',
@@ -8,14 +10,17 @@ import { Product, ProductsService } from '@tutorial/products';
   styles: [
   ]
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, OnDestroy {
 
   products: Product[] = [];
   totalProducts = 0;
-  
 
+  endsubs$: Subject<unknown> = new Subject();
+  
   constructor(
     private productsService: ProductsService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
     private router: Router
   ) { }
 
@@ -23,8 +28,13 @@ export class ProductsListComponent implements OnInit {
     this._getProducts();
   }
 
+  ngOnDestroy(): void {
+    this.endsubs$.next(true);
+    this.endsubs$.complete();
+  }
+
   private _getProducts(){
-    this.productsService.getCategories().subscribe(
+    this.productsService.getCategories().pipe(takeUntil(this.endsubs$)).subscribe(
       data =>{ 
         this.products = data;
         this.totalProducts = data.length;
@@ -41,7 +51,30 @@ export class ProductsListComponent implements OnInit {
   }
 
   deleteProduct(productId: string) {
-
+    this.confirmationService.confirm({
+      message: 'Do you want to Delete this User?',
+      header: 'Delete User',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.productsService.deleteProduct(productId).subscribe(
+          () => {
+            this._getProducts();
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Success',
+              detail: 'User is deleted!'
+            });
+          },
+          () => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: 'User is not deleted!'
+            });
+          }
+        );
+      }
+    });
   }
 
 }
