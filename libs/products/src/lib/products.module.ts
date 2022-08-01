9475bc8c9682230f7f3617579f693d38151394f7
Jose <josebratt@gmail.com>
2022-08-01 12:35:17 -0400

    Update products.module.ts

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -1,22 +1,31 @@
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
 
 import { ProductsSearchComponent } from './components/products-search/products-search.component';
 import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
+
+const routes: Routes = [
+  { path: 'products', component: ProductsListComponent },
+  { path: 'category/:categoryid', component: ProductsListComponent },
+];
 
 @NgModule({
-  imports: [CommonModule, RouterModule, ButtonModule],
-  exports: [ProductsSearchComponent, CategoriesBannerComponent, ProductItemComponent, FeaturedProductsComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), FormsModule, ButtonModule, CheckboxModule],
+  exports: [ProductsSearchComponent, CategoriesBannerComponent, ProductItemComponent, FeaturedProductsComponent, ProductsListComponent],
   declarations: [
     ProductsSearchComponent,
     CategoriesBannerComponent,
     ProductItemComponent,
-    FeaturedProductsComponent
+    FeaturedProductsComponent,
+    ProductsListComponent
   ],
 })
 export class ProductsModule {}
