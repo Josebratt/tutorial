c33c65e20f7d4f124ffa06760f1c065afa90ff08
Jose <josebratt@gmail.com>
2022-08-01 10:10:37 -0400

    Create featured-products.component.ts

---
+++ libs/products/src/lib/components/featured-products/featured-products.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tutorial-featured-products',
+  templateUrl: './featured-products.component.html',
+  styles: [
+  ]
+})
+export class FeaturedProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
