fe2e7bc4d409c38ed852787e5b1db8e213b4fbc9
Jose <josebratt@gmail.com>
2022-08-01 10:46:08 -0400

    Update products.module.ts

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -1,13 +1,16 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ButtonModule } from 'primeng/button';
+
 import { ProductsSearchComponent } from './components/products-search/products-search.component';
 import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ButtonModule],
   exports: [ProductsSearchComponent, CategoriesBannerComponent, ProductItemComponent, FeaturedProductsComponent],
   declarations: [
     ProductsSearchComponent,
