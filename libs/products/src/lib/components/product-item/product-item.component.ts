7707d0bc3982e46a964665898fb7dd1bf40fca31
Jose <josebratt@gmail.com>
2022-08-01 10:46:04 -0400

    Update product-item.component.ts

--- libs/products/src/lib/components/product-item/product-item.component.ts
+++ libs/products/src/lib/components/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'tutorial-product-item',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ProductItemComponent implements OnInit {
+
+  @Input()
+  product: Product = new Product;
 
   constructor() { }
 
