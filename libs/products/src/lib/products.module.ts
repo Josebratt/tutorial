cd7f277323489cb64f84aec12655d63f62a569be
Jose <josebratt@gmail.com>
2022-08-01 10:10:49 -0400

    Update products.module.ts

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsSearchComponent } from './components/products-search/products-search.component';
 import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  exports: [ProductsSearchComponent, CategoriesBannerComponent],
+  exports: [ProductsSearchComponent, CategoriesBannerComponent, ProductItemComponent, FeaturedProductsComponent],
   declarations: [
     ProductsSearchComponent,
-    CategoriesBannerComponent
+    CategoriesBannerComponent,
+    ProductItemComponent,
+    FeaturedProductsComponent
   ],
 })
 export class ProductsModule {}
